74e210216f820f2f58e3922e5cb80e14258dca4c
Geraldine Craig <gerriemcraig@outlook.com>
2020-09-06 00:57:42 -0700

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Footer() {
-    return <div><small><a href="https://github.com/KissaSpirit/weather-react" target="_blank">Open-source code</a>{" "}by SheCodes and Geraldine Craig</small></div>;
+    return <div><small><a href="https://github.com/KissaSpirit/weather-react">Open-source code</a>{" "}by SheCodes and Geraldine Craig</small></div>;
 }
